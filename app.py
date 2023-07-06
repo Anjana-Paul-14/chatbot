8ff31a54c3879aa665bbc0f3b1c3d3f358f30f61
Anjana Paul <anjanapaul0614@gmail.com>
2023-07-06 21:28:40 +0400

    code for chatbot

--- app.py
+++ app.py
@@ -1,1 +1,35 @@
-import
+from flask import Flask, request, render_template
+import os
+import openai 
+
+app = Flask(__name__)
+
+# Configure OpenAI API
+#  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
+openai.api_key = "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"
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    user_input = request.form['user_input']
+    
+    # Call OpenAI API to generate a response
+    response = openai.Completion.create(
+        engine="davinci",
+        prompt=user_input,
+        max_tokens=50,
+        n=1,
+        stop=None,
+        temperature=0.7,
+        top_p=None,
+        frequency_penalty=None,
+        presence_penalty=None,
+        log_level=None
+    )
+
+    chatbot_response = response.choices[0].text.strip()
+    
+    return render_template('chat.html', user_input=user_input, chatbot_response=chatbot_response)
